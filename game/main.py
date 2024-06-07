80f94f9633d32e7aba3a30831855a375793f181d
gregory2023 <gecanoram@gmail.com>
2024-06-07 13:43:40 +0200

    modifico la clase main para su correcta ejecucion

---
+++ game/main.py
@@ -0,0 +1,27 @@
+from jugador import Player
+from game import Game
+
+
+p1 = Player(1, 'a')
+p2 = Player(2, 'b')
+
+
+g1 = Game(p1, p2, 3)
+
+
+print("Datos de los jugadores:")
+print(p1.toString())
+print(p2.toString())
+print()
+
+
+g1.play()
+
+
+winner = g1.winner()
+
+
+if winner is None:
+    print("El juego ha terminado en empate.")
+else:
+    print(f"El ganador es: {winner.toString()}")
